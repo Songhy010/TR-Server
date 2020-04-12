15efcdbd37d907aafefe1e75fe0c63336432660e
Songhy010 <semsonghy071@gmail.com>
2020-04-12 21:55:43 +0700

    fix login

--- app-controllers/controller-teacher.js
+++ app-controllers/controller-teacher.js
@@ -8,30 +8,36 @@ exports.login = async (req,res,next)=>{
         const username = req.body.username;
         const pwd = req.body.pwd;
         const teacher = await model.login(username);
-        const hash = teacher.teacher_password;
-        const isEqual = await bcrypt.compare(pwd,hash);
-
-        if(isEqual){
-            res.send({
-                id:teacher.teacher_id,
-                fname:teacher.teacher_firstname,
-                lname:teacher.teacher_lastname,
-                email:teacher.teacher_email,
-                phone:teacher.teacher_phone,
-                phone1:teacher.teacher_phone1,
-                experience:teacher.teacher_experience,
-                address:teacher.teacher_address,
-                dob:dateFormat(teacher.teacher_dob,"dd/mmmm/yyyy"),
-                date:dateFormat(teacher.teacher_register_date,"dd/mmmm/yyyy"),
-                fname_kh:teacher.teacher_firstname_kh,
-                lname_kh:teacher.teacher_lastname_kh,
-
-            });
+        if(teacher){
+            const hash = teacher.teacher_password;
+            const isEqual = await bcrypt.compare(pwd,hash);
+            if(isEqual){
+                res.send({
+                    id:teacher.teacher_id,
+                    fname:teacher.teacher_firstname,
+                    lname:teacher.teacher_lastname,
+                    email:teacher.teacher_email,
+                    phone:teacher.teacher_phone,
+                    phone1:teacher.teacher_phone1,
+                    experience:teacher.teacher_experience,
+                    address:teacher.teacher_address,
+                    dob:dateFormat(teacher.teacher_dob,"dd/mmmm/yyyy"),
+                    date:dateFormat(teacher.teacher_register_date,"dd/mmmm/yyyy"),
+                    fname_kh:teacher.teacher_firstname_kh,
+                    lname_kh:teacher.teacher_lastname_kh,
+    
+                });
+            }else{
+                const err = Error("Incorrect Usename Password!");
+                err.code = 200;
+                next(err);
+            }
         }else{
             const err = Error("Incorrect Usename Password!");
-            err.code = 200;
-            next(err);
+                err.code = 200;
+                next(err);
         }
+        
     } catch (error) {
         const err = Error("Bad Request!");
         err.code = 400;
